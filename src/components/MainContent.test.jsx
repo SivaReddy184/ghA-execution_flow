168b5097c6c73e67dbe4efe0a3f317d2adc083a7
Siva Reddy <sivareddymanne18@gmail.com>
2024-11-06 10:54:03 +0530

    fixed tests

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -19,6 +19,6 @@ describe('MainContent', () => {
 
     const button = screen.getByRole('button');
     await userEvent.click(button);
-    expect(screen.getByTestId('help-aree')).toBeInTheDocument();
+    expect(screen.getByTestId('help-area')).toBeInTheDocument();
   });
 });
